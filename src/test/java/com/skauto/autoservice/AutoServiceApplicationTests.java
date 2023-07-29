97b45f0331d3ad80cdca5b842abdb15691655d9a
rakesh <rakesh8399@gmail.com>
2023-07-29 11:43:30 +0530

    first commit

---
+++ src/test/java/com/skauto/autoservice/AutoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skauto.autoservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
